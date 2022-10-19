d3ec42dd8baf99294d92704bf097f56b306a341e
Pierre Andrieux <pierre.andrieux@capgemini.com>
2022-10-19 16:37:40 +0200

    weather stack impl

--- WeatherstackTrigger1/__init__.py
+++ WeatherstackTrigger1/__init__.py
@@ -1,24 +1,31 @@
 import logging
+import requests
+import urllib.parse
+import os
 
 import azure.functions as func
 
+access_key=str(os.getenv('weatherstack'))
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    city = req.params.get('city')
+    if not city:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            city = req_body.get('city')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if city:
+        encoded_city=urllib.parse.quote(city)
+        api_url="http://api.weatherstack.com/current?access_key="+access_key+"&query="+encoded_city
+        r = requests.get(url=api_url).json()
+        return func.HttpResponse(f"{r}")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Pass a city in the query string or in the request body for a personalized response.",
              status_code=200
         )
